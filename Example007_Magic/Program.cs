665eb30d72489a0f1e09a585ba0f23b4ada92146
Nadezhda <nadyusha-serdyuk@inbox.ru>
2022-11-29 21:03:38 +0300

    add Example 007 Magic

--- Example007_Magic/Program.cs
+++ Example007_Magic/Program.cs
@@ -2,12 +2,12 @@
 //Console.SetCursorPosition(10, 4);
 //Console.WriteLine("+");
 
-int xa = 1;
+int xa = 25;
 int ya = 1;
 int xb = 1;
-int yb = 30;
-int xc = 40;
-int yc = 30;
+int yb = 20;
+int xc = 50;
+int yc = 20;
 
 Console.SetCursorPosition(xa, ya);
 Console.WriteLine("+");
@@ -17,3 +17,33 @@ Console.WriteLine("+");
 
 Console.SetCursorPosition(xc, yc);
 Console.WriteLine("+");
+
+int x = xa, y = xb;
+
+int count = 10;
+
+while(count < 10000)
+{
+    int what = new Random().Next(0, 3); // [0;3) 0 1 2
+    if(what == 0)
+    {
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
+    }
+
+    if(what == 1)
+    {
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
+    }
+
+    if(what == 2)
+    {
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
+    }
+
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count++;
+}
